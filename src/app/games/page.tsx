4ba0c26815c8beb9fff669ed88ea9a4e3b974292
171-netizen <ridwanadeyemo171@gmail.com>
2026-06-13 14:48:16 +0100

    Fix syntax error with extra div tag

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -216,7 +216,6 @@ function ArcadeHubContent() {
                </button>
              ))}
            </div>
-           </div>
            <div className="flex items-center gap-4">
              <div className="relative">
                <Search className="w-4 h-4 absolute left-3 top-1/2 -translate-y-1/2 text-zinc-400" />
